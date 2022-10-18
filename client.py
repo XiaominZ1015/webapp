5f76a6df7f2ccd0ebb16bf7fa572b69c874f4058
xiaomin1015 <90724226+xiaomin1015@users.noreply.github.com>
2022-10-18 15:24:34 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import psycopg2
 
 con = psycopg2.connect(
-            host = "localhost",
-            database="users-db",
-            user = "webapp",
-            password = "zxm123456")
+            host="localhost",
+            database="users_db",
+            user="webapp",
+            password="zxm123456")
 
 cur = con.cursor()
