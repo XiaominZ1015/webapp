9bd3d4cfdda356151e01bf1acf523de3a7410c79
Xiaomin <>
2022-10-28 16:52:08 -0700

    add endpoints

--- apis/base.py
+++ apis/base.py
@@ -5,4 +5,4 @@ from fastapi import APIRouter
 api_router = APIRouter()
 api_router.include_router(public.router, prefix="/v1", tags=["public"])
 api_router.include_router(unauthenticated.router, prefix="/healthz", tags=["unauthenticated"])
-api_router.include_router(authenticated.router, prefix="/v1/account", tags=["authenticated"])
+api_router.include_router(authenticated.router, prefix="/v1", tags=["authenticated"])
--- database/db.py
+++ database/db.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = "mysql://sherry:123456@127.0.0.1/users_db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://webapp:zxm123456@127.0.0.1/users_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
