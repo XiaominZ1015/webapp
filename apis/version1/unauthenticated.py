8565f0f0f68d778c38c587f3fad2a7bbafd07a25
xiaomin1015 <90724226+xiaomin1015@users.noreply.github.com>
2022-11-11 19:15:40 -0800

    Update unauthenticated.py

--- apis/version1/unauthenticated.py
+++ apis/version1/unauthenticated.py
@@ -2,8 +2,15 @@ from fastapi import APIRouter
 
 
 router = APIRouter()
-
+connection = statsd.Connection(
+       host='127.0.0.1',
+       port=8125,
+       sample_rate=1,
+   )
+client = statsd.Client("python", connection)
+counter = client.get_counter("counter")
 
 @router.get("/")
 def hello():
+    counter += 1
     return {"message": "Hello World"}
