8d524d18f28d8676ccb0b4d3d9aa9524fcbd4655
Xiaomin <>
2022-11-30 14:22:25 -0800

    revise test

--- test_main.py
+++ test_main.py
@@ -5,12 +5,7 @@ from main import app
 client = TestClient(app)
 
 def test_create_user():
-    response = client.post("/v1/account", json={
-      "first_name": "Tony",
-      "last_name": "lu",
-      "email": "tony@111.com",
-      "password": "123"
-    })
+    response = client.get("/healthz")
     assert response.status_code == 200
 
 
