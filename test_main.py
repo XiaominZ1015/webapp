8688661ffd47e1372f5e276f3ad3f6d00a2cd6a6
Xiaomin <>
2022-12-01 14:53:12 -0800

    delete init

--- test_main.py
+++ test_main.py
@@ -1,10 +1,10 @@
 from fastapi.testclient import TestClient
 
-from .main import app
+from main import app
 
 client = TestClient(app)
 
-def test_create_user():
+def test_health():
     response = client.get("/healthz")
     assert response.status_code == 200
 
